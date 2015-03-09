846552915866eb7daee3cc93aec6c931581b892f
David Díaz <diazglezdavid@gmail.com>
2015-03-09 23:58:44 +0000

    Adds several dependencies

--- src/app/index.js
+++ src/app/index.js
@@ -2,7 +2,8 @@
 
 angular.module('wordUp', [
   'ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngRoute', 'mgcrea.ngStrap',
-  'angularMoment', 'angular-lodash'
+  'angularMoment', 'angular-lodash', 'picardy.fontawesome', 'iso-3166-country-codes',
+  'wordUp.forvo'
   ])
   .config(function ($routeProvider) {
     $routeProvider
