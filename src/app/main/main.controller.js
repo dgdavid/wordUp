7a18d8fcc31ede0b7753cc4e0067cf3bf193605d
David Díaz <diazglezdavid@gmail.com>
2015-03-09 23:04:10 +0000

    Provides a simple and initial sketch

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,5 +2,13 @@
 
 angular.module('wordUp')
   .controller('MainCtrl', function ($scope) {
-    $scope.dictionaries = [];
+    var mvm = this;
+    mvm.word = undefined;
+    mvm.dictionaries = [];
+
+    $scope.$watch('mvm.word', function (newValue, oldValue) {
+      if (newValue) {
+        console.info('Word has changed from ' + oldValue + ' to ' + newValue);
+      }
+    });
   });
